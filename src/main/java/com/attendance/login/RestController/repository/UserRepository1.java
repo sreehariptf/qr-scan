e153bf6be11ca9410d3b614c8835b38cc747bcc2
sreehariptf <107536510+sreehariptf@users.noreply.github.com>
2022-07-14 10:20:19 +0530

    Update UserRepository1.java

--- src/main/java/com/attendance/login/RestController/repository/UserRepository1.java
+++ src/main/java/com/attendance/login/RestController/repository/UserRepository1.java
@@ -18,7 +18,7 @@ public interface UserRepository1 extends JpaRepository<User1,Integer> {
 
     List<User1> findByName(String name);
     
-    Iterable<User1> deleteByDate(String time);
+    Iterable<User1> deleteByTime(String time);
     
     
     Boolean existsByPara(String para);
