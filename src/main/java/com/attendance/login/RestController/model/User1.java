36ab9d2d25c412683fe52e5e012231440240a571
sreehariptf <107536510+sreehariptf@users.noreply.github.com>
2022-08-03 13:54:50 +0530

    Update User1.java

--- src/main/java/com/attendance/login/RestController/model/User1.java
+++ src/main/java/com/attendance/login/RestController/model/User1.java
@@ -1,10 +1,12 @@
 package com.attendance.login.RestController.model;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 @Entity
@@ -12,14 +14,19 @@ import java.time.LocalTime;
 
 public class User1 {
 
+
+    public String afternoon="------------";
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    public String forenoon="------------";
     private String name;
     private String email;
     public String para;
     private String phone;
     private LocalDate date=LocalDate.now();
     public String time;
-   // public String month=LocalDate.now().format(DateTimeFormatter.ofPattern("YYYY-MM"));
+
+    public String month=LocalDate.now().format(DateTimeFormatter.ofPattern("YYYY-MM"));
+//    public String time;
 }
