b7ca7307f09185c144620849ec0eec98b67e1ce9
sreehariptf <107536510+sreehariptf@users.noreply.github.com>
2022-08-04 12:39:10 +0530

    Update Controller.java

--- src/main/java/com/attendance/login/RestController/UserControl/Controller.java
+++ src/main/java/com/attendance/login/RestController/UserControl/Controller.java
@@ -48,57 +48,127 @@ public int rsp;
         return verify;
     }
 
-    @PostMapping("/save")
+//     @PostMapping("/save")
+//     public ResponseEntity AddUser(@RequestBody User1 user2) {
+
+// rsp=0;
+
+// //        LocalTime lc= LocalTime.parse(user2.time);
+
+//         if (i.equals(user2.para)) {
+//             System.out.println(user2);
+
+//             if (userRepository1.existsByPara(user2.para)) {
+
+
+//                 return new ResponseEntity<>(HttpStatus.ALREADY_REPORTED);
+//             } else if (user2.time.contains("AM")) {
+//                 System.out.println(user2.time.contains("AM"));
+//                 user2.forenoon = user2.time;
+//                 userRepository1.save(user2);
+//                 rsp=100;
+//                 return new ResponseEntity<>(HttpStatus.OK);
+
+//             } else if (user2.time.contains("PM")) {
+//                 LocalDate date = LocalDate.parse(String.valueOf(LocalDate.now()));
+
+//                 if (userRepository1.existsByDateAndEmail(date, user2.getEmail())) {
+//                     User1 user3;
+//                     user3 = userRepository1.getByDateAndEmail(date, user2.getEmail());
+//                     System.out.println(user3);
+//                     int id = user3.getId();
+//                     user2.setId(id);
+//                     user2.forenoon = user3.forenoon;
+//                     user2.afternoon = user2.time;
+//                     userRepository1.save(user2);
+
+//                     rsp=100;
+//                     System.out.println(rsp);
+//                   return new ResponseEntity<>(HttpStatus.OK);
+
+
+//                 } else {
+//                     user2.afternoon =user2.time;
+//                     userRepository1.save(user2);
+//                     rsp=100;
+//                     return new ResponseEntity<>(HttpStatus.OK);
+//                 }
+//             } else {
+//                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+//             }
+
+//         }
+//         return null;
+//     }
+    
+    
+     @PostMapping("/save")
     public ResponseEntity AddUser(@RequestBody User1 user2) {
 
-rsp=0;
+        rsp = 0;
 
-//        LocalTime lc= LocalTime.parse(user2.time);
+        LocalDate date = LocalDate.now();
 
         if (i.equals(user2.para)) {
-            System.out.println(user2);
 
             if (userRepository1.existsByPara(user2.para)) {
 
 
                 return new ResponseEntity<>(HttpStatus.ALREADY_REPORTED);
+
             } else if (user2.time.contains("AM")) {
-                System.out.println(user2.time.contains("AM"));
-                user2.forenoon = user2.time;
-                userRepository1.save(user2);
-                rsp=100;
-                return new ResponseEntity<>(HttpStatus.OK);
+
+                if (userRepository1.existsByDateAndEmail(date, user2.getEmail())) {
+                    return new ResponseEntity<>(HttpStatus.GONE);
+                } else {
+                    System.out.println(user2.time.contains("AM"));
+                    user2.forenoon = user2.time;
+                    userRepository1.save(user2);
+                    rsp = 100;
+                    return new ResponseEntity<>(HttpStatus.OK);
+                }
 
             } else if (user2.time.contains("PM")) {
-                LocalDate date = LocalDate.parse(String.valueOf(LocalDate.now()));
+//                LocalDate date = LocalDate.parse(String.valueOf(LocalDate.now()));
 
                 if (userRepository1.existsByDateAndEmail(date, user2.getEmail())) {
                     User1 user3;
                     user3 = userRepository1.getByDateAndEmail(date, user2.getEmail());
-                    System.out.println(user3);
-                    int id = user3.getId();
-                    user2.setId(id);
-                    user2.forenoon = user3.forenoon;
-                    user2.afternoon = user2.time;
-                    userRepository1.save(user2);
+                    if (user3.afternoon.equals("------------")) {
+                        System.out.println(user3);
+                        int id = user3.getId();
+                        user2.setId(id);
+                        user2.forenoon = user3.forenoon;
+                        user2.afternoon = user2.time;
+                        userRepository1.save(user2);
 
-                    rsp=100;
-                    System.out.println(rsp);
-                  return new ResponseEntity<>(HttpStatus.OK);
+                        rsp = 100;
+
+                        return new ResponseEntity<>(HttpStatus.OK);
+                    } else
+                        return new ResponseEntity<>(HttpStatus.GONE);
 
 
                 } else {
-                    user2.afternoon =user2.time;
+//                    if (userRepository1.existsByDateAndEmail(date, user2.getEmail())) {
+//
+//                        return new ResponseEntity<>(HttpStatus.GONE);
+//                    } else {
+
+
+                    user2.afternoon = user2.time;
                     userRepository1.save(user2);
-                    rsp=100;
+                    rsp = 100;
                     return new ResponseEntity<>(HttpStatus.OK);
                 }
-            } else {
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-
             }
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 
         }
+
+//    }
         return null;
     }
 
